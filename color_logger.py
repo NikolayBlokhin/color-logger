4f05e6fde748ec7ff148e238051f9a52d54018a4
Nikolay Blokhin <nikolay@blokhin.us>
2017-04-12 09:20:05 +0200

    Logging only to console

--- color_logger.py
+++ color_logger.py
@@ -9,26 +9,29 @@ from termcolor import cprint
 
 class ColorLogger(object):
     """
-    Write log messages to file and
-    console (with any colors and backgrounds)
+    Write log messages to console (with any colors and
+    backgrounds) and to file (optional).
     """
 
-    def __init__(self, logger_name, log_file_name):
-        super(SmartLogger, self).__init__()
+    def __init__(self, logger_name=None, log_file_name=None):
+        super(ColorLogger, self).__init__()
 
-        self.logger_name = logger_name
-        self.log_file_name = log_file_name
+        self.logger = None
 
-        self.logging_formatter = logging.Formatter(
-            '[%(asctime)s][%(levelname)s] %(message)s',
-            '%Y.%m.%d %H:%M:%S',
-        )
+        if logger_name and log_file_name:
+            self.logger_name = logger_name
+            self.log_file_name = log_file_name
 
-        handler = get_rotation_handler(log_file_name)
-        self.logger = logging.getLogger(logger_name)
-        self.logger.setLevel(logging.DEBUG)
-        self.logger.propagate = False
-        self.logger.addHandler(handler)
+            self.logging_formatter = logging.Formatter(
+                '[%(asctime)s][%(levelname)s] %(message)s',
+                '%Y.%m.%d %H:%M:%S',
+            )
+
+            handler = get_rotation_handler(log_file_name)
+            self.logger = logging.getLogger(logger_name)
+            self.logger.setLevel(logging.DEBUG)
+            self.logger.propagate = False
+            self.logger.addHandler(handler)
 
     def get_rotation_handler(log_file_name):
         rotating_handler = logging.handlers.RotatingFileHandler(
@@ -41,7 +44,8 @@ class ColorLogger(object):
         return rotating_handler
 
     def write(self, message, color=None, background=None, attrs=[]):
-        self.logger.debug(message)                         # write to file
         cprint(message, color, background, attrs=attrs)    # write to console
+        if self.logger:
+            self.logger.debug(message)                     # write to file
 
 
